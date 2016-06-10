c8d5cdc2e591e81a37cb0d21dd8b6f6b3f7a1d84
Richard Paquin <richard-paquin@users.noreply.github.com>
2016-06-10 18:35:20 +0200

    adding codility_print

---
+++ codility_print.py
@@ -0,0 +1,9 @@
+def printable(func):
+    def print_wrapper(*args):
+        _x = func(*args)
+        s_var = ', '.join(repr(arg) for arg in args)
+        if len(s_var) > 50:
+            s_var = s_var[0:25] + '..........' + s_var[-15:len(s_var)]
+        print "In:{} Out:{}".format(s_var, _x)
+        return _x
+    return print_wrapper    
