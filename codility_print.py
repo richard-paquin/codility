76206190b2a35306aa6d83270c7847866f79891b
Richard Paquin <richard-paquin@users.noreply.github.com>
2016-06-11 11:28:48 +0200

    added docstring

--- codility_print.py
+++ codility_print.py
@@ -1,4 +1,6 @@
 def printable(func):
+    """Execute the function and then print a string containing the args and the
+    return value."""
     def print_wrapper(*args):
         _x = func(*args)
         s_var = ', '.join(repr(arg) for arg in args)
@@ -6,4 +8,4 @@ def printable(func):
             s_var = s_var[0:25] + '..........' + s_var[-15:len(s_var)]
         print "In:{} Out:{}".format(s_var, _x)
         return _x
-    return print_wrapper    
+    return print_wrapper
