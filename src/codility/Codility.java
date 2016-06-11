5f5c76e745f714a19b6f54d788e42dd93027f911
Richard Paquin <richard-paquin@users.noreply.github.com>
2016-06-11 14:19:25 +0200

    Main

---
+++ src/codility/Codility.java
@@ -0,0 +1,16 @@
+package codility;
+
+/**
+ *
+ * @author Dick84
+ */
+public class Codility {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
